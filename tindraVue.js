be013b63bc4fd7fe856519e72cb9a5c815d9cc74
Tindra98 <t.jutterstrom@gmail.com>
2024-10-02 11:03:47 +0200

    tindras sida

---
+++ tindraVue.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            projects: [
+                
+            ]
+        }
+    },
+    created() {
+        axios.get('tindraNotes.json')
+            .then((response) => {
+                this.projects = response.data.projects; // Assign the projects array
+            })
+    }
+});
+
+app.mount('#app');
